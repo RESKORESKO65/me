d9cabae04fc18e4d7dbc8376552b03f714e6e9ea
moshaks <43033517+moshaks@users.noreply.github.com>
2018-10-14 16:45:45 -0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+
+client.on("ready", () => {
+console.log('By : moshaks');
+client.user.setPresence({
+  status: 'dnd',
+  game: {
+     type: 0,
+     name: 'Owner ,',
+     details: `Clan`,
+     url: 'http://twitch.tv/moshaks-Dev',
+     state: `Crazyskills`,
+    application_id: '377479790195769345',
+     assets: {
+        small_image: `377480550207717376`,
+        small_text: 'تعرف مشاكس',
+        large_image: `377480353259978752`,
+        large_text: `IMrz ّζ͜͡MOSHADev` }
+
+  }
+    });
+});
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
